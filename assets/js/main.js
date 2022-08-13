8406f28c92e5a61649b3834beb5ae10ed4c0a6af
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-08-13 12:55:50 -0500

    this clases

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
 console.log("Entro al main.js");
+class Perro {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
+    ladrar() {
+        console.log(`$ {this.nombre} esta ladrando`);
+    }
+    sentar() {
+        console.log(this.nombre +"esta sentado");
+    }
+        
+}
+
+let perro_firulais= new Perro("Firulais");
+perro_firulais.ladrar();
+perro_firulais.sentar();
+console.log("------------------");
+
+let perro_dalmata= new Perro("Dalmata");
+perro_dalmata.ladrar();
+perro_dalmata.sentar();
+console.log("------------------");
