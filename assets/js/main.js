a66e8a620239ddf7bc68ad8a43589adacb88827c
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-08-17 09:22:22 -0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,53 @@ class Perro {
         console.log(`$ {this.nombre} esta ladrando`);
     }
     sentar() {
-        console.log(this.nombre +"esta sentado");
+        console.log(this.nombre + "esta sentado");
     }
-        
+
 }
 
-let perro_firulais= new Perro("Firulais");
+let perro_firulais = new Perro("Firulais");
 perro_firulais.ladrar();
 perro_firulais.sentar();
 console.log("------------------");
 
-let perro_dalmata= new Perro("Dalmata");
+let perro_dalmata = new Perro("Dalmata");
 perro_dalmata.ladrar();
 perro_dalmata.sentar();
 console.log("------------------");
+
+
+//ejemplo de herencia 
+
+class Persona {
+    // constructor(nombre,apellido,edad,genero,intereses){
+
+    // }
+    constructor(nombre, apellido, edad, genero, intereses) {
+        this.edad = edad;
+        this.genero = genero;
+        this.intereses = intereses;
+        this.nombre_completo = {
+            nombre: nombre,
+            apellido: apellido
+        };
+    }
+    saludo() {
+        console.log("hola soy la clase persona");
+    }
+    despedida() {
+        console.log(`la persona ${this.nombre_completo.nombre`});
+        
+}
+}
+let maestro=new Persona("jesus",
+"cardena","29","maesculino",
+["programacion","acuarofilia","ajedrez"]);
+let alumno=new Persona("juan",
+"cardena","29","maesculino",
+["futball","golf","ajedrez"]);
+
+alumno.saludo();
+
+
+
